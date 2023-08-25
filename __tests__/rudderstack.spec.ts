880be416a4fab923c0f345441b36a09926558f21
amir ali <amir.ali@regentmarkets.com>
2023-08-25 18:29:32 +0500

    feat: add support for tracking anonymous user

--- __tests__/rudderstack.spec.ts
+++ __tests__/rudderstack.spec.ts
@@ -1,3 +1,4 @@
+import * as RudderAnalytics from "rudder-sdk-js";
 import { RudderStack } from "../src/rudderstack";
 
 jest.mock("rudder-sdk-js", () => {
@@ -22,8 +23,14 @@ describe("rudderstack", () => {
       RUDDERSTACK_STAGING_KEY: "123456789",
       RUDDERSTACK_URL: "http://example.com",
     };
+  });
 
+  beforeEach(() => {
     rudderstack = new RudderStack();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   afterAll(() => {
@@ -43,6 +50,10 @@ describe("rudderstack", () => {
   });
 
   test("should not be empty if current page is passed", () => {
+    rudderstack.identifyEvent("C123", {
+      language: "en",
+    });
+
     rudderstack.pageView("app.deriv.com");
 
     expect(rudderstack.current_page).not.toBe("");
@@ -61,5 +72,36 @@ describe("rudderstack", () => {
     rudderstack.reset();
 
     expect(rudderstack.has_identified).toBe(false);
+  });
+
+  test("should call RudderAnalytics track function if is_anonymous pass to true", () => {
+    const analyticsSpy = jest.spyOn(RudderAnalytics, "track");
+
+    rudderstack.track(
+      "ce_trade_types_form",
+      {
+        action: "open",
+      },
+      { is_anonymous: true }
+    );
+
+    expect(analyticsSpy).toHaveBeenCalledTimes(1);
+    expect(analyticsSpy).toHaveBeenCalledWith("ce_trade_types_form", {
+      action: "open",
+    });
+  });
+
+  test("should not call RudderAnalytics track function if not identified and is not anonymous", () => {
+    const analyticsSpy = jest.spyOn(RudderAnalytics, "track");
+
+    rudderstack.track(
+      "ce_trade_types_form",
+      {
+        action: "open",
+      },
+      { is_anonymous: false }
+    );
+
+    expect(analyticsSpy).not.toHaveBeenCalled();
   });
 });
