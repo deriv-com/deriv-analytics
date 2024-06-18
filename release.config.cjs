987bfaeaa7d3a4af893600e217a901789a9972f9
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-06-18 14:41:43 +0800

    ci: update the release branch

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,7 @@
 module.exports = {
     branches: [
         "+([0-9])?(.{+([0-9]),x}).x",
-        "main",
+        "master",
         "next",
         "next-major",
         { name: "beta", prerelease: true },
