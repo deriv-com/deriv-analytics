7bb687456f65322c9b7900b9d7a014a63319a6bb
Shayan <iman@regentmarkets.com>
2025-08-28 16:18:28 +0800

    chore: testing my gh ssh key on new laptop

--- src/analytics.ts
+++ src/analytics.ts
@@ -15,6 +15,7 @@ type Options = {
     rudderstackKey: string
     growthbookOptions?: TGrowthbookOptions
     disableRudderstackAMD?: boolean
+    //test
 }
 
 export function createAnalyticsInstance(options?: Options) {
