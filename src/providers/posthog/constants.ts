8c0e1f0bcdd7ac482cf79a1934c2b533041707a1
Niloofar <niloofar.sadeghi@regentmarkets.com>
2026-01-29 15:49:05 +0800

    fix: removed keys

--- src/providers/posthog/constants.ts
+++ src/providers/posthog/constants.ts
@@ -1,6 +1,3 @@
-export const POSTHOG_STAGING_API_KEY = 'phc_rWYGnxz2HJ6umy5W7XgfiBYSvMrA4eBB8hmYNjkgsTI'
-export const POSTHOG_PRODUCTION_API_KEY = 'phc_AgTRVvFy6RB726wjSAbOIRsAv4483iFBIKaaICXFbML'
-
 export const POSTHOG_API_HOST = 'https://ph.deriv.com'
 export const POSTHOG_UI_HOST = 'https://us.posthog.com'
 
